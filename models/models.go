7aadd40f008e65b3c6b3aa9f6d7791a1f20c1035
afonso-borges <dev.joao@protonmail.com>
2023-04-22 06:21:59 -0300

    fix: fixed "exp" json parse

--- models/models.go
+++ models/models.go
@@ -11,7 +11,7 @@ type Character struct {
 	ID            int64  `json:"id"`
 	CharacterName string `json:"CharacterName"`
 	Level         int    `json:"level"`
-	Exp           int    `json:"exp amount"`
+	Exp           int    `json:"exp"`
 	GuildIn       string `json:"guild"`
 }
 
